2f63da5fb81d115dbe3e4e35daf6086460233add
Jessica Giliam <jessicagiliam922@gmail.com>
2024-05-23 22:53:07 -0700

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,11 @@
+/* JGiliam: Module 3 step 3 Read data created in trips.json */
+var fs = require('fs');
+var path = require('path');
+var trips = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'trips.json'), {encoding: 'utf8', flag: 'r'}));
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways'});
+    res.render('travel', { title: 'Travlr Getaways', trips});
 };
 
 module.exports = {
